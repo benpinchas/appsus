a32ee062fb58c55f0c3e965331b2609baa2317a5
Ben Pinchas <benphs5@gmail.com>
2019-06-20 15:59:23 +0300

    composer component start, menu component added

---
+++ js/apps/email/cmps/email-composer.cmp.js
@@ -0,0 +1,12 @@
+'use strict';
+
+export default {
+    name: 'emailComposer',
+    template: `
+          <div class="email-composer">
+               <button><img src="/media/colored-plus.png"></img> Compose</button>
+          </div>
+      `,
+  };
+
+  console.log('Email composer file loaded');
--- js/apps/email/cmps/email-menu.cmp.js
+++ js/apps/email/cmps/email-menu.cmp.js
@@ -1,13 +1,19 @@
 
+import emailComposer from './email-composer.cmp.js'
 
 export default {
     name: 'emailMenu',
     template: `
           <section class="email-menu">
-                menu1
-                menu2
-                menu3
-                menu4
+                <main>
+                  <email-composer></email-composer>
+                  <router-link to="/email"><i class="fas fa-inbox"></i> Incoming</router-link>
+                  <router-link to="/starred"><i class="fas fa-star"></i> Starred</router-link>
+                  <router-link to="/sent"><i class="fas fa-paper-plane"></i> Sent</router-link>
+                </main>
           </section>
       `,
+      components: {
+        emailComposer
+      }
   };
