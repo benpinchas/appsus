3bcde3621dd836f848362ca96da66647e43e2323
Ben Pinchas <benphs5@gmail.com>
2019-06-20 13:20:29 +0300

    email preview component

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -1,11 +1,39 @@
 'use strict';
 
+
 export default {
   name: 'emailList',
   template: `
         <div class="email-preview">
-              {{email.body}}
+              <div class="name">
+                  {{email.from.name}}
+              </div>
+              <div class="subject-body">
+                <span class="subject">{{email.subject}}</span> 
+                -
+                <span class="body">{{email.body}}</span> 
+              </div>
+              <div class="time">
+                {{fDate}}
+              </div>
         </div>
     `,
-  props: ['email']
+  props: ['email'],
+  computed: {
+    fDate() {
+      let date = new Date(this.email.sentAt)
+      new Date().toLocaleDateString()
+      return date.toString().slice(16,21) +'  '+ date.toLocaleDateString()
+    }
+  }
+
 };
+
+
+/*
+ computed: {
+    fDate() {
+      return 5//new Date(this.email)
+    }
+  }
+*/
--- js/apps/email/services/email.service.js
+++ js/apps/email/services/email.service.js
@@ -12,21 +12,33 @@ gEmails = [
     subject: 'First email',
     body: 'Bla bla bla',
     isRead: false,
-    sentAt: Date.now()
+    sentAt: Date.now() - 300000,
+    from: {
+      name: 'ehud',
+      email: 'ehud@gmail.com'
+    }
   },
   {
     id: Math.random() + '',
     subject: '2 email',
     body: 'Bla bla bla',
     isRead: false,
-    sentAt: Date.now()
+    sentAt: Date.now() - 10000,
+    from: {
+      name: 'ben',
+      email: 'ben@gmail.com'
+    }
   },
   {
     id: Math.random() + '',
     subject: '3 email',
     body: 'Na Bla bla bla',
     isRead: false,
-    sentAt: Date.now()
+    sentAt: Date.now() -123123123123,
+    from: {
+      name: 'tal',
+      email: 'tal@gmail.com'
+    }
   }
 ];
 
