5c07a417a8897eaf5f8b0f1521624dfb478bb749
Ben Pinchas <benphs5@gmail.com>
2019-06-20 22:19:23 +0300

    class-binding email-preview CMP

--- js/apps/email/cmps/email-preview.cmp.js
+++ js/apps/email/cmps/email-preview.cmp.js
@@ -4,12 +4,19 @@
 export default {
   name: 'emailList',
   template: `
-        <div class="email-preview">
+        <div class="email-preview" v-bind:class="{'unread': !email.isRead}" v-if="email">
               <div class="name">
-                  <span @click="putStar" v-if="isIncomeMail">
-                    <i v-if="email.isStarred" style="color:#f7d51c;" class="fas fa-star"></i>
-                    <i v-else class="far fa-star" style="color:#b6b4b4"></i>
-                  </span>
+                  <span v-if="isIncomeMail" class="star-read-container">
+                    <span @click="toggleStar">
+                      <i v-if="email.isStarred" style="color:#f7d51c;" class="fas fa-star"></i>
+                      <i v-else class="far fa-star" style="color:#b6b4b4"></i>
+                    </span>  
+                    
+                    <span @click="toggleRead">
+                      <i class="far fa-envelope-open" v-if="email.isRead"></i>
+                      <i class="fas fa-envelope" v-else></i>
+                    </span>
+                </span>
                   {{email.contact.name}}
               </div>
               <div class="subject-body">
@@ -27,15 +34,24 @@ export default {
     fDate() {
       let date = new Date(this.email.sentAt)
       new Date().toLocaleDateString()
-      return date.toString().slice(16,21) +'  '+ date.toLocaleDateString()
+      return date.toString().slice(16, 21) + '  ' + date.toLocaleDateString()
     },
     isIncomeMail() {
       return this.$route.params.theFilter !== "sent";
     }
   },
   methods: {
-    putStar() {
+    classObject () {
+      return {
+        'unread': !this.email.isRead,
+        'unre': false,
+      }
+    },
+    toggleStar() {
       this.email.isStarred = !this.email.isStarred
+    },
+    toggleRead() {
+      this.email.isRead = !this.email.isRead
     }
   }
 
