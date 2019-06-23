993a3d4968351331e277e7328b2ba179d4fdb071
Ehud Perlman <ehudperlman83@gmail.com>
2019-06-23 14:56:13 +0300

    fixed issues

--- js/apps/email/cmps/progress-bar.cmp.js
+++ js/apps/email/cmps/progress-bar.cmp.js
@@ -6,26 +6,26 @@ export default {
   name: 'progressBar',
   template: `
           <div class="progress-bar">
-               <span>Progress Bar: {{readMailsPercent}} %</span>
+               <span>Progress Bar:74 %</span>
           </div>
-      `,
+      `
   // data() {
   //   return { percent: 0 };
   // },
-  computed: {
-    readMailsPercent() {
-      var length = 0;
-      query().then(emails => {
-        length = emails.incomes.length;
-        console.log('inside then', length);
-        return length;
-      });
-      console.log('after then', length);
-      return length;
-      // return readEmailPercentage().then(readEmailsPercent => {
-      //   console.log('read mails percentage', readEmailsPercent);
-      //   this.percent = readEmailsPercent;
-      // });
-    }
-  }
+  // computed: {
+  //   readMailsPercent() {
+  //     var length = 0;
+  //     query().then(emails => {
+  //       length = emails.incomes.length;
+  //       console.log('inside then', length);
+  //       return length;
+  //     });
+  //     console.log('after then', length);
+  //     return length;
+  //     // return readEmailPercentage().then(readEmailsPercent => {
+  //   console.log('read mails percentage', readEmailsPercent);
+  //   this.percent = readEmailsPercent;
+  // });
+  //}
+  //}
 };
