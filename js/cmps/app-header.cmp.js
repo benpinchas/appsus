2861eeccac9f9394a52e6739341d6d8918abe200
Ehud Perlman <ehudperlman83@gmail.com>
2019-06-24 17:22:16 +0300

    added links to mail from home and to homefrom logo

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -8,13 +8,18 @@ export default {
         <main>
           <div>
         <i class="fas fa-horse-head"></i>
-        <span>Appsus</span>
+        <span @click="goToHomePage">Appsus</span>
         </div>
         <app-navigator></app-navigator>
         <mini-list></mini-list>
     </main>
     </section>
     `,
+  methods: {
+    goToHomePage() {
+      this.$router.push('/');
+    }
+  },
   components: {
     appNavigator,
     miniList
