b3b0ecd735948a4cfcdec50117d05404e9a3633c
Ben Pinchas <benphs5@gmail.com>
2019-06-24 22:43:11 +0300

    progress bar line

--- js/apps/email/cmps/progress-bar.cmp.js
+++ js/apps/email/cmps/progress-bar.cmp.js
@@ -6,7 +6,14 @@ export default {
   name: 'progressBar',
   template: `
           <div class="progress-bar" style="" v-if="emails">
-               <span>Progress Bar: {{readedPercent}} %</span>
+               <!-- <span>Progress Bar: {{readedPercent}} %</span> -->
+               <div style="width:100%; background-color:#d6d6d6; border-radius: 5px;">
+               <!-- {{readedPercent}} -->
+                <div class="progress-line"
+                style="background-color:#0084ff; padding: 2px;
+                  border-radius: 5px; color:white;" :style="{transform: 'scaleX('+sacleX+')'}">
+                  </div>
+                </div>
           </div>
       `,
   props: ['emails'],
@@ -14,6 +21,10 @@ export default {
     readedPercent() {
         let readedCount = this.emails.incomes.filter(email => email.isRead)
         return parseInt(readedCount.length /  this.emails.incomes.length * 100)
+    },
+    sacleX() {
+      let readedCount = this.emails.incomes.filter(email => email.isRead)
+      return readedCount.length /  this.emails.incomes.length
     }
   }
 
