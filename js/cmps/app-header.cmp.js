4111487d9f8f96a1123d06aa7723aa0246a0eac8
Ehud Perlman <ehudperlman83@gmail.com>
2019-06-24 17:13:39 +0300

    added logo

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,12 +1,15 @@
 import appNavigator from './navigator.cmp.js';
-import miniList from '../apps/email/cmps/minified-email-list.cmp.js'
+import miniList from '../apps/email/cmps/minified-email-list.cmp.js';
 
 export default {
   name: 'appHeader',
   template: `
     <section class="app-header">
         <main>
-        General Appsus header CMP
+          <div>
+        <i class="fas fa-horse-head"></i>
+        <span>Appsus</span>
+        </div>
         <app-navigator></app-navigator>
         <mini-list></mini-list>
     </main>
