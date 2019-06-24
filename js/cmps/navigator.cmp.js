451bcf07b6bf77c7cbf629c51e17780f2dd0c583
Ben Pinchas <benphs5@gmail.com>
2019-06-24 20:00:08 +0300

    navigator-window transition

--- js/cmps/navigator.cmp.js
+++ js/cmps/navigator.cmp.js
@@ -6,12 +6,13 @@ export default {
     <div class="navigator">
         <button @click="toggleNavigationWindow"><i class="fas fa-th"></i></button>
         
+        <transition name="fade">
         <div class="navigation-window" v-if="isOpen" @click=" toggleNavigationWindow">
             <router-link exact to="/"><i class="fas fa-home"></i></router-link>
             <router-link  to="/email"><i class="fas fa-envelope-open-text"></i></router-link>
             <router-link  to="/keep"><i class="fas fa-sticky-note"></i></router-link>
         </div>    
-        
+        </transition>
     </div>
     `,
   data() {
